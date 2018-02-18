6a47e8d6c40a30be8137634be63cd1757e6e4f08
Anatoliy <anatoly.podkladov@finalprice.com>
2018-02-18 19:17:37 +0300

    added config.swift with api id & hash

---
+++ Config.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  Telegram
+//
+//  Created by Kant on 13.01.2018.
+//  Copyright © 2018 Telegram. All rights reserved.
+//
+
+import Foundation
+
+
+let API_ID:Int32 = 23217
+let API_HASH:String = "fab3290cedc15950eca33e1937e5423b"
+let TEST_SERVER:Bool = false
+let languagesCategory = "macos"
